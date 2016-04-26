e619fa3348f6f54bd19f484adfbc8f71e1547ab0
Timothy M. Crider <tcrider@sevone.com>
2016-04-26 09:38:21 -0400

    Bringing environment inside

--- ci-post-build.sh
+++ ci-post-build.sh
@@ -8,7 +8,22 @@
 # Load the build environment
 #
 #MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#source ./build-tools/build-env.sh
 
-source ./build-tools/build-env.sh
+#
+# Decorations
+#
+export THIN_LINE="--------------------------------------------------------------------------------"
+export THICK_LINE="==============================================================================="
+
+printHeader () {
+	echo
+	echo $THIN_LINE
+	echo "$1"
+	echo $THIN_LINE
+	echo
+}
+
+export -f printHeader
 
 printHeader " Post Build Phase "
