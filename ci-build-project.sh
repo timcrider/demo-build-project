c77a4dd13000188c4d2c516a929b4f020f8ab9dd
Timothy M. Crider <tcrider@sevone.com>
2016-04-26 09:36:18 -0400

    Removing auto detect for demo

--- ci-build-project.sh
+++ ci-build-project.sh
@@ -7,8 +7,8 @@
 #
 # Load the build environment
 #
-MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $MY_DIR/build-tools/build-env.sh
+source ./build-tools/build-env.sh
 
 printHeader " Execute Build Phase "
